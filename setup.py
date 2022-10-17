161d9d8886acd45b23bdd86633e8954191ddb3f3
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2022-10-17 22:58:58 +0800

    alvistack/0.52.0
        git clean -xdf
        tar zcvf ../python-antsibull_0.52.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-antsibull.spec ../python-antsibull_0.52.0-1.spec
        mv ../python*-antsibull*0.52.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/ansible-community-antsibull-0.52.0/
        rm -rf ../python*-antsibull*0.52.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['antsibull', 'antsibull.cli', 'antsibull.data', 'antsibull.utils']
+
+package_data = \
+{'': ['*'], 'antsibull.data': ['debian/*']}
+
+install_requires = \
+['antsibull-changelog>=0.14.0',
+ 'antsibull-core>=1.3.0,<3.0.0',
+ 'asyncio-pool',
+ 'jinja2']
+
+entry_points = \
+{'console_scripts': ['antsibull-build = antsibull.cli.antsibull_build:main',
+                     'antsibull-lint = antsibull.cli.antsibull_lint:main']}
+
+setup_kwargs = {
+    'name': 'antsibull',
+    'version': '0.52.0',
+    'description': 'Tools for building the Ansible Distribution',
+    'author': 'Toshio Kuratomi',
+    'author_email': 'a.badger@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/ansible-community/antsibull',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
